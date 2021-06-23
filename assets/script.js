4724948481f4229ee46f7b51fb858f13c0e51665
Mohammad Zafar <sz7861@outlook.com>
2021-06-23 11:47:53 +0100

    add main variables including time and score

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var intro = document.querySelector(".criteriapage");
+var crit = document.querySelector(".quiz-criteria");
+var startBtn = document.querySelector("startedBtn");
+var timer = document.getElementById("timer");
+var answers = document.querySelectorAll("#questArea button");
+var time = 100;
+var score = 0;
+var quizCount = 0;
+var recordArray = [];
+var userScores = [];
+var clock;
+var setTime;
