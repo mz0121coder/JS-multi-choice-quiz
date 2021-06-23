5426c0e193c702adcf16d064a8d18e0bdadac64a
Mohammad Zafar <sz7861@outlook.com>
2021-06-23 11:52:15 +0100

    add achieved score + timeout elements

--- assets/script.js
+++ assets/script.js
@@ -22,4 +22,24 @@ var pageContentEl = function (element) {
     });
     pageContentEl(element).classList.remove("hidden");
   };
-  
+  //achieved score
+var quizUpdate = function (answerCopy) {
+    pageContentEl("#achievedScore p").innerHTML = answerCopy;
+    pageContentEl("#achievedScore").classList.remove("gone", achievedScore());
+    Array.from(answers).forEach((answer) => {
+      answer.classList.add("disable");
+    });
+      //quiz completed
+  setTimeout(function () {
+    if (quizCount === questions.length) {
+      onlyDisplaySection("#finish");
+      time = 0;
+      pageContentEl("#time").innerHTML = time;
+    } else {
+      setQuestionData();
+      Array.from(answers).forEach((answer) => {
+        answer.classList.remove("disable");
+      });
+    }
+  }, 1000);
+};
