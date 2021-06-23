82ddc4c7a6d10c84c34af6e89d57fd37969a0b01
Mohammad Zafar <sz7861@outlook.com>
2021-06-23 12:35:43 +0100

    add 5 'fill in the gap' style questions

--- assets/script.js
+++ assets/script.js
@@ -118,7 +118,36 @@ var counterTimer = function () {
         choices: ["toSource()", "sort()", "toString()", "unshift()"],
         answer: "sort()",
     }, 
-    
+    {
+        questionInfo: "Commonly Used Data types do NOT include",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts",
+    },
+    {
+        questionInfo:
+          "The condition in an if / else statement is enclosed within _________",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses",
+    },
+    {
+        questionInfo: "Arrays in JavaScript can be used to store ________",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above",
+        ],
+        answer: "all of the above",
+    },
+    {
+        questionInfo:
+          "String values must be enclosed within ________ when being assigned to variables",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes",
+    },
+    {
+        questionInfo:
+          "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+        answer: "console.log",
+    },
+    ];
     
     
 
